9e1235b30e16cc6f5118557534d9e0391ddbf6f2
조형준 <89668546+Tojaman@users.noreply.github.com>
2024-02-15 01:16:42 +0900

    feat : 동영상 변환 요청 프록시 API 구현(Spring)

--- backend/src/main/java/com/example/CustomLecture/dto/Request/VideoSaveRequestDTO.java
+++ backend/src/main/java/com/example/CustomLecture/dto/Request/VideoSaveRequestDTO.java
@@ -20,22 +20,4 @@ public class VideoSaveRequestDTO {
     private String subject;
     private String thumbnailS3Path;
     private String lectureMaterialsS3Path;
-
-//    private String subtitleS3Path;
-
-
-//    private String originalS3Path;
-
-
-    // DB에서 자동 생성
-//    private Date date;
-
-    // s3 스토리지에 저장
-//    private Blob lectureNote;
-//    private Blob thumbnail;
-
-    // 백엔드에서 변환 작업 후 VideoPostResponseDTO에서 사용
-//    private String convertS3Path;
-
-
 }
