2ebb379db6bfaa9c9adb6827dd09397ae9637e1b
조형준 <89668546+Tojaman@users.noreply.github.com>
2024-02-15 01:49:10 +0900

    refactor : 강의 영상 업로드 API swagger 적용

---
+++ backend/src/main/java/com/example/CustomLecture/dto/Request/VideoConvertRequestDTO.java
@@ -0,0 +1,14 @@
+package com.example.CustomLecture.dto.Request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class VideoConvertRequestDTO {
+    private Integer userId;
+    private String url;
+    private String rvcModel;
+}
