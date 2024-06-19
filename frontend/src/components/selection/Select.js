7cbd1084fcd33174bb9cce5841810076be77669c
hsyou1129 <ghtjs7155@gmail.com>
2024-06-20 01:06:29 +0900

    fix: 이미지 및 select 수정

--- frontend/src/components/selection/Select.js
+++ frontend/src/components/selection/Select.js
@@ -14,8 +14,8 @@ import avatarImg4 from "../../assets/avatarImg/트럼프.jpg";
 import avatarImg5 from "../../assets/avatarImg/키키.jpg";
 import avatarImg6 from "../../assets/avatarImg/뽀로로.jpg";
 
-import voiceImg1 from "../../assets/avatarImg/문재인.jpg";
-import voiceImg2 from "../../assets/avatarImg/일론머스크.jpg";
+import voiceImg1 from "../../assets/avatarImg/정우성.jpg";
+import voiceImg2 from "../../assets/avatarImg/지민.jpg";
 import voiceImg3 from "../../assets/avatarImg/아이유.jpg";
 import voiceImg4 from "../../assets/avatarImg/카리나.jpg";
 
@@ -69,19 +69,11 @@ const CustomSlider = styled(Slider)`
   }
 
   .slick-slide {
-    margin: 0 10px;
-
-    &:hover {
-      height: 150px;
-    }
+    padding-top: 10px;
   }
+
   .slick-track {
     display: flex;
-  }
-
-  .selected {
-    height: 150px;
-    border: 2px solid #ffeb0b;
   }
 
   .slick-prev:before,
@@ -97,6 +89,16 @@ const Img = styled.img`
   margin: 0 auto 5px;
   display: block;
   border-radius: 50%;
+  transition: box-shadow 0.3s ease-in-out;
+
+  &:hover {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  }
+  
+  &.selected {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.7);
+  }
+
 `;
 
 const Name = styled.div`
@@ -198,29 +200,33 @@ const Select = () => {
       <ContentContainer>
         <SelectText>음성 선택</SelectText>
         <Selection>
-          <CustomSlider {...settings}>
-            {voices.map((voice, index) => (
-              <div key={voice.name} className={selectedVoiceIndex === index ? "selected" : ""} onClick={() => handleVoiceSelection(index)}>
-                <Img src={voice.img} />
-                <Name>{voice.name}</Name>
-              </div>
-            ))}
-          </CustomSlider>
+        <CustomSlider {...settings}>
+  {voices.map((voice, index) => (
+    <div key={voice.name} onClick={() => handleVoiceSelection(index)}>
+      <Img
+        src={voice.img}
+        alt={voice.name}
+        className={selectedVoiceIndex === index ? 'selected' : ''}
+      />
+      <Name>{voice.name}</Name>
+    </div>
+  ))}
+</CustomSlider>
         </Selection>
         <SelectText>아바타 선택</SelectText>
         <Selection>
-          <CustomSlider {...settings}>
-            {avatars.map((avatar, index) => (
-              <div
-                key={avatar.name}
-                className={selectedAvatarIndex === index ? "selected" : ""}
-                onClick={() => handleAvatarSelection(index)}
-              >
-                <Img src={avatar.img} />
-                <Name>{avatar.name}</Name>
-              </div>
-            ))}
-          </CustomSlider>
+        <CustomSlider {...settings}>
+  {avatars.map((avatar, index) => (
+    <div key={avatar.id} onClick={() => handleAvatarSelection(index)}>
+      <Img
+        src={avatar.img}
+        alt={avatar.name}
+        className={selectedAvatarIndex === index ? 'selected' : ''}
+      />
+      <Name> {avatar.name}</Name>
+    </div>
+  ))}
+</CustomSlider>
         </Selection>
 
         <NextButton onClick={goToCameraPage}>
