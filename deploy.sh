02976a958eb6ff7330a34e6ed8d16d3b745e4e75
Tojaman <zzoon5020@tukorea.ac.kr>
2024-09-17 20:28:42 +0000

    devops: github action 테스트

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# 최신 코드 가져오기
+git pull origin serverTest/#1
+
+# Docker Compose 작업
+sudo docker-compose down
+sudo docker-compose build
+sudo docker-compose up -d
+
+# 불필요한 Docker 이미지 삭제
+sudo docker image prune -f
